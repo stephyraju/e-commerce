6678f66801c04ae6627b73690e3d4b09f4f4d34e
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2019-12-04 22:33:26 +0000

    heroku name added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C2_HOSTNAME'),'stephy-ecommerce.herokuapp.com']
 ALLOWED_HOSTS =['0.0.0.0']
 
 # Application definition
